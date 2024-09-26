6e80df5410a75230d8f7d71c310cdb2ae75ed824
antonioneto2 <antonio.nog28@gmail.com>
2024-09-26 14:45:37 -0300

    add protótipos das funções

--- main.c
+++ main.c
@@ -17,3 +17,22 @@ typedef struct horario {
     struct horario* esquerda;
     struct horario* direita;
 } Horario;
+
+// Protótipo da função adicionarHorario
+Horario* adicionarHorario(Horario* node, int dia, int hora); 
+void salvarHorarios(Horario* root, FILE* arquivo);
+void salvarFuncionarios();
+Horario* carregarHorarios(Horario* root);
+void carregarFuncionarios();
+int altura(Horario* N);
+int max(int a, int b);
+Horario* novoHorario(int dia, int hora);
+Horario* rotacaoDireita(Horario* y);
+Horario* rotacaoEsquerda(Horario* x);
+int getBalance(Horario* N);
+int verificarHorario(Horario* node, int dia, int hora);
+void mostrarHorarios(Horario* root);
+void listarServicos();
+void marcarHorario(Horario** root);
+void cadastrarFuncionario();
+void profissionaisDisponiveis();
