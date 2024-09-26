146a535e56aa9e2e9b8308cebcc9a9c94bfcb4e4
antonioneto2 <antonio.nog28@gmail.com>
2024-09-26 14:44:36 -0300

    add structs

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define MAX_FUNCIONARIOS 10 // Máximo de funcionários
+
+// Definição de estruturas
+typedef struct funcionario {
+    char nome[50];
+    char cargo[50];
+} Funcionario;
+
+typedef struct horario {
+    int diaSemana;
+    int horario;
+    int altura;
+    struct horario* esquerda;
+    struct horario* direita;
+} Horario;
