2c24eecd7f934f42ca06900c5d5f516a4fd48edd
antonioneto2 <antonio.nog28@gmail.com>
2024-09-29 12:20:23 -0300

    add função para salvar horarios no arquivo

--- src/horario.c
+++ src/horario.c
@@ -2,3 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "/home/neto/Documentos/Projeto/include/horario.h"
+
+// Função para salvar todos os horários em um arquivo
+void salvarHorarios(Horario* root, FILE* arquivo) {
+    if (root != NULL) {
+        salvarHorarios(root->esquerda, arquivo);
+        fprintf(arquivo, "Dia %d, Horario: %d:00, Cliente: %s, Serviço: %s\n", 
+            root->diaSemana, root->horario, root->nomeCliente, root->servico); // Salva dia e horário no arquivo
+        salvarHorarios(root->direita, arquivo);
+    }
+}
