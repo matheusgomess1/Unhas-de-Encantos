ceba571a7154b49a7ef91b9bfc4fb680623358ae
antonioneto2 <antonio.nog28@gmail.com>
2024-10-03 12:14:59 -0300

    Nova função listarSevicos

--- src/servico.c
+++ src/servico.c
@@ -8,3 +8,27 @@ void inicializarTabelaHash(TabelaHash tabela[]) {
     tabela[3] = (TabelaHash){4, "Alongamento de Unhas", {"Tips", "Gel", "Lixa Elétrica"}, 3};
     tabela[4] = (TabelaHash){5, "Nail Art", {"Pincéis", "Pedrinhas", "Adesivos"}, 3};
 }
+
+// Função para listar os serviços e perguntar se o usuário quer ver os insumos
+void listarServicos(TabelaHash tabela[]) {
+    printf("\n===== LISTA DE SERVIÇOS =====\n");
+    for (int i = 0; i < NUM_SERVICOS; i++) {
+        printf("%d - %s\n", tabela[i].id, tabela[i].nomeServico);
+    }
+    printf("==============================\n");
+
+    // Pergunta ao usuário se ele deseja ver os insumos de algum serviço
+    char escolha;
+    printf("Você deseja ver os insumos de algum serviço? (s/n): ");
+    scanf(" %c", &escolha);  // Espaço antes de %c para ignorar qualquer caractere residual no buffer
+
+    if (escolha == 's' || escolha == 'S') {
+        int idServico;
+        printf("Digite o ID do serviço para ver os insumos: ");
+        if (scanf("%d", &idServico) == 1) {
+            listarInsumosPorServico(tabela, idServico);
+        } else {
+            printf("ID inválido. Por favor, tente novamente.\n");
+        }
+    }
+}
